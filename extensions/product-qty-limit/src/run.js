08090e6ac0fdec522bbee958bf5713a86135ecf9
Manoj bhatt <bhatmanoj97@gmail.com>
2024-06-03 17:27:54 +0530

    first commit

---
+++ extensions/product-qty-limit/src/run.js
@@ -0,0 +1,28 @@
+// @ts-check
+
+/**
+ * @typedef {import("../generated/api").RunInput} RunInput
+ * @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
+ */
+
+/**
+ * @param {RunInput} input
+ * @returns {FunctionRunResult}
+ */
+export function run(input) {
+  const errors = input.cart.lines
+    .filter(({ quantity, merchandise }) => {
+      if (merchandise.__typename === "ProductVariant") {
+        const limit = merchandise.product.metafield?.value;
+        return limit && quantity > Number(limit);
+      }
+    })
+    .map(() => ({
+      localizedMessage: "Not possible to order more than one of each",
+      target: "cart",
+    }));
+
+  return {
+    errors,
+  };
+}
